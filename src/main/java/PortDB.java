9e0fe562b6286ef02f2872b43b4a28e5352e3ca2
willu2 <bob@example.com>
2019-04-02 01:12:44 +0300

    temp1

--- src/main/java/PortDB.java
+++ src/main/java/PortDB.java
@@ -11,13 +11,13 @@ public class PortDB {
         String[][] portArray;
         int k = 0;
         for(int i = 0; i < ports.length ; i++){
-            for(int p = 0; p < ports.length ; p++){
-                if(i == p) continue;
+            for(int p = i + 1 ; p < ports.length ; p++){
                 k++;
+                //System.out.println(k);
                 //System.out.println(ports[i] + " ----->>> "+ ports[p] +"  _______"+ p );
             }
         }
-        portArray = new String[k][colums];
+        portArray = new String[k-1][colums];
        /* for (int i = 0; i < priceList.length; i++) {
             // Цикл по второй размерности выводит колонки - вывод одной строки
             for (int j = 0; j < priceList[i].length; j++) {
@@ -27,28 +27,32 @@ public class PortDB {
 */        return portArray;
     }
 
-    ;//{{from, to, weight, price}}
+    //{{from, to, weight, price}}
 
     public void makePortsMap(String[][] portArray){
 
         int raw = 0;
         int colum = 0;
-
+        int h = 1;
         for(int from = 0; from < ports.length ; from++){
-            for(int to = 0; to < ports.length ; to++){
-                if(from == to) continue;
+            for(int to = from + 1 ; to < ports.length ; to++){
 
                 portArray[raw][colum] = ports[from];//your value
                 colum++;
+
                 portArray[raw][colum] = ports[to];
                 colum --;
-                //System.out.println(ports[i] + " ----->>> "+ ports[p] +"  _______"+ p );
-                raw++;
-            }
-        }
-        addPrice(portArray);
 
-        printArrayMap(portArray);
+                System.out.println(ports[from] + " <<<----->>> "+ ports[to] +"  _______" + h );
+                h++;
+            }
+                raw++;
+        }
+
+        System.out.println(h);
+       // addPrice(portArray);
+
+       // printArrayMap(portArray);
 
     }
 
@@ -63,22 +67,47 @@ public class PortDB {
         }
     }
 
+    public void sameData(String[][] priceList){
+        for (int i = 0; i < priceList.length; i++) {
+
+            for (int j = 0; j < priceList[i].length - 2; j++) {
+                // Используем оператор print - без перехода на следующую строку
+                if(priceList[i][j] != null){
+
+                    System.out.println("bopa"/*priceList[i][j]*/);
+                }
+
+            }
+        }
+    }
     public void addPrice(String[][] priceList){
         int raw = 0;
-        int colum = 0;
+        //int colum = 0;
         Random random = new Random();
         for (int i = 0; i < priceList.length; i++) {
-            for (int j = 2; j < priceList[i].length; j++) {
+            int colum = 0;
+            for (int j = 3; j < priceList[i].length; j++) {
                 // Используем оператор print - без перехода на следующую строку
-                if(j==2){
-                    priceList[i][j] = String.valueOf(random.nextInt(15000)+2500);
-                }else if(j==3){
-                    priceList[i][j] = String.valueOf(random.nextInt(1000)+100);
-                }else {
+               // sameData(priceList);
+
+               // System.out.println(priceList[i][colum]);
+                colum++;
+                if (j == 2) {
+                    priceList[i][j] = String.valueOf(random.nextInt(15000) + 2500);
+                } else if (j == 3) {
+                    priceList[i][j] = String.valueOf(random.nextInt(1000) + 100);
+                } else {
                     priceList[i][j] = "_44__ ";
                 }
             }
         }
     }
 }
+
+
+
+
+
+
+
 
